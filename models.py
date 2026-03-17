2003505fd51d0e31787eb6e30c6081aa06da7f14
marca29 <marca7298@gmail.com>
2026-03-17 20:53:42 +0100

    Created InsurancePolicy Model, added validation for policy dates, handled CamelCase input data

--- models.py
+++ models.py
@@ -1,9 +1,15 @@
-from pydantic import BaseModel, Field, EmailStr, computed_field, field_validator
+from pydantic import BaseModel, Field, EmailStr, computed_field, field_validator, model_validator
+from pydantic import ConfigDict
 from enum import Enum
 from decimal import Decimal
-from datetime import datetime
+from datetime import datetime, date
 from uuid import UUID
 import re
+
+def to_camel(string: str) -> str:
+    parts = string.split("_")
+    return parts[0] + "".join(word.capitalize() for word in parts[1:])
+
 
 class Currency(str, Enum):
     USD = "USD"
@@ -14,12 +20,20 @@ class TransactionType(str, Enum):
     DEBIT = "DEBIT"
     CREDIT = "CREDIT"
 
+class PolicyStatus(str, Enum):
+    ACTIVE = "ACTIVE"
+    ELAPSED = "ELAPSED"
+    PENDING = "PENDING"
+
 class Address(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
+    street: str
     street: str
     city: str
     zip_code: str = Field(pattern=r"^\d{5}$")
 
 class User(BaseModel):
+    model_config = ConfigDict(alias_generator=to_camel, populate_by_name=True)
     id: str = Field(min_length=8)
     email: EmailStr
     age: int = Field(ge=18, le=120)
@@ -58,3 +72,21 @@ class Transaction(BaseModel):
         if self.amount > 5000:
             return "Medium"
         return "Low"
+    
+class InsurancePolicy(BaseModel):
+    policy_number: str = Field(min_length=10, max_length=10)
+    start_date: date
+    end_date: date
+    status: PolicyStatus
+
+    @field_validator("policy_number")
+    def uppercase(cls, v):
+        if not v.isupper():
+            raise ValueError("Policy number must be uppercase")
+        return v
+
+    @model_validator(mode="after")
+    def validate_dates(self):
+        if (self.end_date - self.start_date).days < 30:
+            raise ValueError("Policy must last at least 30 days")
+        return self
